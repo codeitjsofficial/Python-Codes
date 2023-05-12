68cb2525f786393414aee4f99fa9f72fc3be14f4
codeitjs <132351050+codeitjsofficial@users.noreply.github.com>
2023-05-13 01:25:36 +0530

    Added countdown_timer.py 🚀

---
+++ YT/countdown_timer.py
@@ -0,0 +1,14 @@
+import time
+
+def countdown(t):
+  while t > 0:
+    print(f"{t} seconds remaining.")
+    time.sleep(1)
+    t -= 1
+
+  print("Done!")
+
+
+def main():
+  time_to_countdown = int(input("Enter the time to count down from in seconds: "))
+  countdown(time_to_countdown)
