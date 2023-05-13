cd7a5fe51d239e078f1ecf577e3c4d980d2c6351
codeitjs <132351050+codeitjsofficial@users.noreply.github.com>
2023-05-14 00:07:43 +0530

    Added is_armstrong.py 🚀

---
+++ is_armstrong.py
@@ -0,0 +1,17 @@
+def is_armstrong_number(number):
+    num_str = str(number)
+    num_digits = len(num_str)
+    
+    armstrong_sum = sum(int(digit) ** num_digits for digit in num_str)
+    
+    if armstrong_sum == number:
+        return True
+    else:
+        return False
+
+
+number = int(input("Enter a number: "))
+if is_armstrong_number(number):
+    print(number, "is an Armstrong number.")
+else:
+    print(number, "is not an Armstrong number.")
